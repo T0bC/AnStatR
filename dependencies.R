acc1437d630198dbd74e199d7cf323b5dd0e9c14
T0bC <59888795+T0bC@users.noreply.github.com>
2026-02-06 14:41:52 +0100

    chore: add cli library to dependencies for deployment compatibility

--- dependencies.R
+++ dependencies.R
@@ -1,6 +1,7 @@
 # This file allows packrat (used by rsconnect during deployment) to pick up dependencies.
 library(bsicons)
 library(bslib)
+library(cli)
 library(DT)
 library(openxlsx)
 library(rhino)
