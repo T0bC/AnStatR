7c47811ceaa4c6eff67975e5e6d5f2e7e57c3f2c
T0bC <59888795+T0bC@users.noreply.github.com>
2026-02-06 13:57:34 +0100

    feat: implement main app structure with bslib navbar and load data module integration

--- app/main.R
+++ app/main.R
@@ -1,25 +1,30 @@
 box::use(
-  shiny[bootstrapPage, div, moduleServer, NS, renderUI, tags, uiOutput],
+  bslib,
+  shiny,
+)
+
+box::use(
+  app/view/load_data,
 )
 
 #' @export
 ui <- function(id) {
-  ns <- NS(id)
-  bootstrapPage(
-    uiOutput(ns("message"))
+  ns <- shiny$NS(id)
+
+  bslib$page_navbar(
+    id = ns("active_page"),
+    title = "TexAn 2.0",
+    bslib$nav_panel(
+      title = "Load Data",
+      value = "load_data",
+      load_data$ui(ns("load_data"))
+    )
   )
 }
 
 #' @export
 server <- function(id) {
-  moduleServer(id, function(input, output, session) {
-    output$message <- renderUI({
-      div(
-        style = "display: flex; justify-content: center; align-items: center; height: 100vh;",
-        tags$h1(
-          tags$a("Check out Rhino docs!", href = "https://appsilon.github.io/rhino/")
-        )
-      )
-    })
+  shiny$moduleServer(id, function(input, output, session) {
+    load_data_result <- load_data$server("load_data")
   })
 }
