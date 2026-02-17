8feeb665876c98731af295a02876b6ae89ffbdcc
T0bC <59888795+T0bC@users.noreply.github.com>
2026-02-17 14:41:14 +0100

    refactor: add cluster_error_parser to cluster logic exports

--- app/logic/cluster/__init__.R
+++ app/logic/cluster/__init__.R
@@ -1,6 +1,10 @@
 #' @export
 box::use(
-  app/logic/cluster/cluster[validate_inputs, run_clustering],
+  app/logic/cluster/cluster[
+    validate_inputs,
+    run_clustering,
+    cluster_error_parser,
+  ],
   app/logic/cluster/hopkins[compute_hopkins],
   app/logic/cluster/optimal_clusters[
     compute_optimal_clusters,
