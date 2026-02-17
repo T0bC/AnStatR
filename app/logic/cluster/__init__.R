385dfdec52ad2c2f1339c2843631fc6017cc2889
T0bC <59888795+T0bC@users.noreply.github.com>
2026-02-17 14:21:39 +0100

    refactor: add create_optimal_clusters_ggplot function to cluster logic exports

--- app/logic/cluster/__init__.R
+++ app/logic/cluster/__init__.R
@@ -2,5 +2,8 @@
 box::use(
   app/logic/cluster/cluster[validate_inputs, run_clustering],
   app/logic/cluster/hopkins[compute_hopkins],
-  app/logic/cluster/optimal_clusters[compute_optimal_clusters],
+  app/logic/cluster/optimal_clusters[
+    compute_optimal_clusters,
+    create_optimal_clusters_ggplot,
+  ],
 )
