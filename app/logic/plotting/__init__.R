a5a4051b68a82e74f6a9fa6ce9adee0eef04bad3
T0bC <59888795+T0bC@users.noreply.github.com>
2026-05-22 11:45:03 +0200

    feat: add plotting module exports for plot factory and helper functions

--- app/logic/plotting/__init__.R
+++ app/logic/plotting/__init__.R
@@ -1,1 +1,28 @@
+box::use(
+  app/logic/plotting/plot_factory,
+  app/logic/plotting/plot_helpers,
+)
 
+#' @export
+create_plot <- plot_factory$create_plot
+
+#' @export
+PLOT_TYPES <- plot_factory$PLOT_TYPES
+
+#' @export
+get_plot_type_choices <- plot_factory$get_plot_type_choices
+
+#' @export
+shows_points <- plot_factory$shows_points
+
+#' @export
+shows_stat_overlays <- plot_factory$shows_stat_overlays
+
+#' @export
+is_boxplot_type <- plot_factory$is_boxplot_type
+
+#' @export
+is_violin_type <- plot_factory$is_violin_type
+
+#' @export
+create_empty_plot <- plot_helpers$create_empty_plot
