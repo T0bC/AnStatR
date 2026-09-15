077cb81d0ea851a227a18bf4212ac76e3237cc52
T0bC <59888795+T0bC@users.noreply.github.com>
2026-09-15 15:26:14 +0200

    Simplify nolint comment in LDA diagnostics

--- app/logic/lda/lda_diagnostics.R
+++ app/logic/lda/lda_diagnostics.R
@@ -36,7 +36,7 @@ compute_pooled_vc <- function(scores, groups) {
     idx <- which(groups == g)
     n_g <- length(idx)
     if (n_g < 2) next
-    g_scores <- scores[idx, , drop = FALSE] # nolint: unused_declared_object_linter.
+    g_scores <- scores[idx, , drop = FALSE] # nolint
     g_cov <- stats::cov(g_scores)
     pooled <- pooled + (n_g - 1) * g_cov
     total_df <- total_df + (n_g - 1)
