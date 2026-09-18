f6255b0c84955ae3ce7de9aebba1d5005fee37ec
T0bC <59888795+T0bC@users.noreply.github.com>
2026-09-18 14:30:37 +0200

    Add imputation functions to preprocessing module exports

--- app/logic/preprocessing/__init__.R
+++ app/logic/preprocessing/__init__.R
@@ -1,5 +1,13 @@
 #' @export
 box::use(
+  app/logic/preprocessing/impute[
+    assess_imputation,
+    build_impute_spec,
+    choose_impute_ncomp,
+    default_na_cap_percent,
+    impute_error_parser,
+    impute_missing
+  ],
   app/logic/preprocessing/na_handling[analyse_na, clean_na_rows],
   app/logic/preprocessing/normalize[
     get_transform_label,
